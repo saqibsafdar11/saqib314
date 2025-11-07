ecf85fa4a0ac75cedb22876a866f936e64bfefd2
Saqib Safdar <saqib.safdar@gmail.com>
2025-11-07 22:08:49 +0000

    Configure correct base path for GitHub Pages project repository

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://saqibsafdar11.github.io',
+  base: '/saqib314/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
