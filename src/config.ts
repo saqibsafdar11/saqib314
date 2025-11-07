ffad35ae45232dcc9200670f2a58ba90d85565b5
Saqib Safdar <saqib.safdar@gmail.com>
2025-11-07 22:03:43 +0000

    Initialize Astrofy portfolio for Saqib Safdar with CV and projects

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Saqib Safdar | Data Scientist & AI Specialist';
+export const SITE_DESCRIPTION = 'Data scientist, educator, and AI specialist focused on problem-solving and data-driven solutions. Expertise in strategic leadership and educational technology implementation.';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
